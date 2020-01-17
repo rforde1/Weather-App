821db7eac808d5aa86d01a10a7aeaf56580fcf56
ramiforde <ramiforde@gmail.com>
2020-01-17 12:08:22 -0500

    Updated styles to page and created on click function for searched button. Still working on querying the api for current weather bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var container = $(".container");
 
 function creatElements(){
 
+
     var row1 = $("<div>").addClass("row");
     var row2 = $("<div>").addClass("row");
     $(".container").append(row1);
@@ -13,7 +14,7 @@ function creatElements(){
     row1.append(pageTitle);
 
 // Search city div to search all cities 
-    var citySearchDiv = $("<div>").addClass("col-md-3 border border-primary city-search-div");
+    var citySearchDiv = $("<div>").addClass("col-md-12 city-search-div");
     citySearchDiv.text("Search For a City : ")
     // Input area for user to type city of choice
     var citySearch = $("<input>");
@@ -21,16 +22,26 @@ function creatElements(){
     // Button to click which will later have event-listener
     var searchBtnEl = $("<button>").addClass("btn btn-info");
     searchBtnEl.text("Search");
+// Created div to hold all searched city buttons
+    var allSearchedCitiesDiv = $("<div>");
+    var allSearchedCitiesHeader = $("<h4>").addClass("searched-header");
+    allSearchedCitiesDiv.append(allSearchedCitiesHeader);
+    allSearchedCitiesHeader.text("Cities you have searched: ");
+// Break to variable to help with layout of page
+
+    var br = $("<br>");
+    allSearchedCitiesDiv.append(br);
     
     citySearchDiv.append(citySearch);
     citySearchDiv.append(searchBtnEl);
+    citySearchDiv.append(allSearchedCitiesDiv);
 
-    // Area of page where 
-    var cityIndex = $("<div>").addClass("col-md-8 border border-primary city-index");
-
+    var cityIndex = $("<div>").addClass("col-md-12 city-index");
+    
     row2.append(citySearchDiv);
     row2.append(cityIndex);
-
+    
+    // Area of page where currentday and 5 day forecast will render
     var cityInfo = $("<div>").addClass("col-md-12 city-info");
     cityInfo.text("City Info :")
     var cityForecast = $("<div>").addClass("col-md-12 city-forecast");
@@ -39,9 +50,48 @@ function creatElements(){
     cityIndex.append(cityInfo);
     cityIndex.append(cityForecast);
 
-    
+    var searchCities = $("<button>");
+        
+    // Based on user input once search button is clicked generate button to toggle city index(currentWeather , 5 day forecast)
+    searchBtnEl.on("click", function(){
+        console.log("on click worked");
+         searchCities.text(citySearch.val());
+        allSearchedCitiesDiv.append(searchCities);
+        console.log("It worked");
+    });
+    // On click on city button will render response data for cityInfo
+    searchCities.on("click", function(){
+         
+        //  Create variable to caputure button value
+        var city = searchCities.val();
+
+        var key = "d7341b8b46766c6ab58b9ea3476b4103";
+        // Then we want to grab api used to display response data with city location being button value
+        var queryURL ="https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + key;
+
+         $.ajax({
+            url:queryURL,
+            method:"GET"
+          }).then(function(response){
+            console.log(response);
+          })
+    });
+}
+creatElements();
+
+
+
+// api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+// Get current City searched data
+
+
+
+function getCityCurrent(){
     
 }
+// Get current city search 5 day
+function getFiveDay(){
 
-creatElements();
+}
+
 
